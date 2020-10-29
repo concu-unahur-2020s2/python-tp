7c228715b95078bdc63d71a5915e228f892acb1e
adrian <agyaniri@gmail.com>
2020-10-29 18:14:58 -0300

    algo armado

--- 1.py
+++
@@ -1,8 +0,0 @@
-class Vuelo:
-    def __init__(self,nro):
-        self.nroVuelo = nro
-
-
-vuelo1 = Vuelo(50)
-
-print(vuelo1.nroVuelo)
--- ParteBasica.py
+++ ParteBasica.py
@@ -3,33 +3,42 @@ import threading
 import random
 import time
 
-
-
-logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S',
+                    level=logging.INFO)
 
 
 class Heladera:
 
-    def __init__(self,cantLatasMax,cantBotellasMax):
+    def __init__(self, cantLatasMax, cantBotellasMax):
         self.cantLatasMax = cantLatasMax
         self.cantBotellaMax = cantBotellasMax
         self.cantTotalLatas = 0
         self.cantTotalBotellas = 0
 
-
-    def estaLLena(self):
+    def hayLugarLatas(self):
         return self.cantTotalLatas <= self.cantLatasMax
 
-    def ponerLatas(self, cant):
-        if not self.estaLLena():
-            print('esta llena')
-            self.cantTotalLatas += cant
+    def ponerLata(self):
+        self.cantTotalLatas += 1
 
-    def ponerBotellas(self,cant):
-        self.cantTotalBotellas += cant
+    def ponerBotella(self, cant):
+        self.cantTotalBotellas += 1
 
-heladera = Heladera(15,10)
-heladera.ponerLatas(18)
+    def llenarHeladeraConLatas(self, cant):
+        cantAPoner = cant
+        sobrante = self.cantLatasMax - cant
+
+        while cantAPoner > 0:
+            if self.hayLugarLatas():
+                self.ponerLata()
+                cantAPoner -= 1
+            else:
+                raise Exception('no hay mas lugar')
+
+
+
+heladera = Heladera(15, 10)
 print(heladera.cantTotalLatas)
-print(heladera.estaLLena())
-
+heladera.llenarHeladeraConLatas(17)
+print(heladera.hayLugarLatas())
+print(heladera.cantTotalLatas)
