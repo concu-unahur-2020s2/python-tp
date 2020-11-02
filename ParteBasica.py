ed33db85197850e9edff54154ee0d81b713dfa84
adrian <agyaniri@gmail.com>
2020-11-02 00:26:48 -0300

    revisar

--- ParteBasica.py
+++ ParteBasica.py
@@ -5,41 +5,58 @@ import time
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S',
                     level=logging.INFO)
-
+latasSobrantes = 0
+botellasSobrantes = 0
+cantLatas = 10
+cantBotellas = 4
 semaforoHeladera = threading.Semaphore()
 
-class Heladera:
+
+class Heladera(threading.Thread):
 
     def __init__(self, cantLatasMax, cantBotellasMax):
+        super().__init__()
         self.cantLatasMax = cantLatasMax
         self.cantBotellaMax = cantBotellasMax
         self.cantTotalLatas = 0
         self.cantTotalBotellas = 0
 
-    def hayLugarLatas(self):
-        return self.cantTotalLatas <= self.cantLatasMax
+    def cantidadEspacioDiponible(self):
+        return self.cantLatasMax - self.cantTotalLatas
 
     def ponerLata(self):
         self.cantTotalLatas += 1
 
-    def ponerBotella(self, cant):
+    def ponerBotella(self):
         self.cantTotalBotellas += 1
 
     def llenarHeladeraConLatas(self, cant):
         cantAPoner = cant
-        sobrante = self.cantLatasMax - cant
-
-
-        while self.cantTotalLatas <= self.cantLatasMax:
-        #while cantAPoner > 0 :
-            if self.hayLugarLatas():
+        global latasSobrantes
+        latasSobrantes = cantAPoner - self.cantTotalLatas
+        while cantAPoner > 0:
+            if self.cantidadEspacioDiponible() > 0:
                 self.ponerLata()
-
-            else:
-                raise Exception('no hay mas lugar en la heladera....')
             cantAPoner -= 1
+            logging.info("poniendo una lata")
+            time.sleep(random.randint(1, 2))
 
+    def llenarHeladeraConBotellas(self, cant):
+        cantAPoner = cant
+        global botellasSobrantes
+        botellasSobrantes = cantAPoner - self.cantTotalBotellas
+        while cantAPoner > 0:
+            if self.cantidadEspacioDiponible() > 0:
+                self.ponerBotella()
+            cantAPoner -= 1
+            logging.info("poniendo botellas")
+            time.sleep(random.randint(1, 2))
 
+    def run(self):
+        semaforoHeladera.acquire()
+
+        self.llenarHeladeraConBotellas(15)
+        self.llenarHeladeraConLatas(10)
 
 
 class Proveedor(threading.Thread):
@@ -48,15 +65,8 @@ class Proveedor(threading.Thread):
         super().__init__()
         self.nombre = nombre
 
-
-    #def run(self):
-
-
-
+    # def run(self):
 
 
 heladera = Heladera(15, 10)
-print(heladera.cantTotalLatas)
-heladera.llenarHeladeraConLatas(18)
-print(heladera.hayLugarLatas())
-print(heladera.cantTotalLatas)
+heladera.start()
